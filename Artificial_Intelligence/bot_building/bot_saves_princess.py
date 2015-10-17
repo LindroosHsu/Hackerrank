a79b8d7770b9e3e95186f825ebf32a9e68a6e923
Lindroos Hsu <emptysmile1013@gmail.com>
2015-10-18 05:09:45 +0800

    change bot saves princess into object-oriented for further problems usage.

--- Artificial_Intelligence/bot_building/bot_saves_princess.py
+++ Artificial_Intelligence/bot_building/bot_saves_princess.py
@@ -2,40 +2,75 @@
 
 #!/usr/bin/python
 
-m = int(input())
-grid = list(list(input().strip()) for _ in range(m))
-bot_pos = {'x':0, 'y':0}
-pri_pos = {'x':0, 'y':0}
-
-#finding princess and bot position
-for i in range(m):
-    for j in range(m):
-        if grid[i][j] == 'p':
-            pri_pos['x'] = j
-            pri_pos['y'] = i
-            
-        elif grid[i][j] == 'm':
-            bot_pos['x'] = j
-            bot_pos['y'] = i
-            
-        if bot_pos['x'] != 0 and pri_pos['x'] != 0:
+class Node:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def set_x(self, x):
+        self.x = x
+    
+    def set_y(self, y):
+        self.y = y
+    
+    def get_x(self):
+        return self.x
+        
+    def get_y(self):
+        return self.y
+    
+    def distance(self, other):
+        return abs(self.x - other.x) + abs(self.y - other.y)
+     
+def finding_pri_bot_pos(grid, grid_size):
+    bot_pos = Node(0, 0)
+    pri_pos = Node(0, 0)
+    found_pri = False
+    found_bot = False
+    
+    #finding princess and bot position
+    for i in range(grid_size):
+        for j in range(grid_size):
+            if grid[i][j] == 'p':
+                pri_pos.set_x(j)
+                pri_pos.set_y(i)
+                found_pri = True
+                
+            elif grid[i][j] == 'm':
+                bot_pos.set_x(j)
+                bot_pos.set_y(i)
+                found_bot = True
+                
+            if found_bot and found_pri:
+                break
+        if found_bot and found_pri:
             break
-    if bot_pos['x'] != 0 and pri_pos['x'] != 0:
-        break
+            
+    return pri_pos, bot_pos
+    
+def next_step(bot_x, bot_y, pri_x, pri_y):
+    if bot_x > pri_x:
+        for _ in range(bot_x - pri_x):
+            print("LEFT")
 
-if bot_pos['x'] > pri_pos['x']:
-    for step in range(bot_pos['x']-pri_pos['x']):
-        print("LEFT")
-        
-elif bot_pos['x'] < pri_pos['x']:
-    for step in range(pri_pos['x']-bot_pos['x']):
-        print("RIGHT")
-        
-if bot_pos['y'] > pri_pos['y']:
-    for step in range(bot_pos['y']-pri_pos['y']):
-        print("UP")
-        
-elif bot_pos['y'] < pri_pos['y']:
-    for step in range(pri_pos['y']-bot_pos['y']):
-        print("DOWN")
+    else:
+        for _ in range(pri_x - bot_x):
+            print("RIGHT")
+
+    if bot_y > pri_y:
+        for _ in range(bot_y - pri_y):
+            print("UP")
+
+    else:
+        for _ in range(pri_y - bot_y):
+            print("DOWN")
+            
+
+if __name__ == "__main__":
+    m = int(input())
+    grid = list(list(input().strip()) for _ in range(m))
+    pri_loc_file_name = "princess_location"
+
+    pri_pos, bot_pos = finding_pri_bot_pos(grid, m)
+    next_step(bot_pos.get_x(), bot_pos.get_y(), pri_pos.get_x(), pri_pos.get_y())
 
