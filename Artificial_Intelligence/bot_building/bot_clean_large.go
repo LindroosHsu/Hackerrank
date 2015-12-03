8fc9296660382543473cd0e20d90889ee7e3c977
Lindroos Hsu <emptysmile1013@gmail.com>
2015-12-04 02:29:43 +0800

    TODO: trying to use backtrack way and save result into file then skiping calculate the solve path everytimes

--- Artificial_Intelligence/bot_building/bot_clean_large.go
+++ Artificial_Intelligence/bot_building/bot_clean_large.go
@@ -9,36 +9,53 @@ type Position struct {
 	x, y int
 }
 
-func NewPosition(setX, setY int) *Position {
-	return &Position{x:setX, y:setY}
+type DirtyNode struct {
+    Position
+    dis int
 }
 
-// scaning grid to find all dirties and check is the bot
-// standing on a dirty
-func findDirties(bot *Position, dirties []Position) bool {
+func NewPosition(setX, setY int) *Position {
+	return &Position{setX, setY}
+}
+
+func NewDirtyNode(setX, setY, distance int) *DirtyNode {
+    return &DirtyNode{Position{setX, setY}, distance}
+}
+
+// 1. scaning grid to find all dirties and check is the bot standing on a dirty
+// 2. calculate the shortest disctance then return the nearby dirty
+func findDirties(bot *Position) *DirtyNode {
 	var h, w int
 	fmt.Scanf("%d %d\n", &h, &w)
 
-	for i := 0; i < h+1; i++ {
-		for j := 0; j < w+1; j++ {
+    // the dirty should be cleaned means the shortest distance dirty
+    theDirty := *NewDirtyNode(0, 0, h*w)
+	for dirtyY := 0; dirtyY < h; dirtyY++ {
+		for dirtyX := 0; dirtyX < w+1; dirtyX++ {
 			var input int
 			fmt.Scanf("%c", &input)
 
 			if input == 'd' {
-				if bot.x == j && bot.y == i {
+				if bot.x == dirtyX && bot.y == dirtyY {
                     // bot is standing on a dirty
-                    return true
+                    return nil
                 }
 
-                dirties = append(dirties, *NewPosition(j, i))
+                // calculate the distance from bot to this dirty
+                distance := int(math.Abs(float64(dirtyX-bot.x)) + math.Abs(float64(dirtyY-bot.y)))
+                if distance < theDirty.dis {
+                    theDirty.dis = distance
+                    theDirty.x = dirtyX
+                    theDirty.y = dirtyY
+                }
 			}
 		}
 	}
 
-    return false
+    return &theDirty
 }
 
-func printNextStep(bot, dirty *Position) {
+func printNextStep(bot *Position, dirty *DirtyNode) {
 	if bot.x > dirty.x {
 		fmt.Println("LEFT")
 
@@ -53,22 +70,18 @@ func printNextStep(bot, dirty *Position) {
 	}
 }
 
-func findShortDirty()
-
 func main() {
     var botX, botY int
 
     // in hackerrank X and Y are reversed...
     fmt.Scanf("%d %d\n", &botY, &botX)
-	bot := *NewPosition(botX, botY)
 
-    var dirties []Position
-	onClean := findDirties(dirties)
-
-    if onClean {
+	bot := NewPosition(botX, botY)
+	dirty := findDirties(bot)
+    if dirty == nil {
         fmt.Println("CLEAN")
         return
     }
 
-    printNextStep(&bot, &pri)
+    printNextStep(bot, dirty)
 }
