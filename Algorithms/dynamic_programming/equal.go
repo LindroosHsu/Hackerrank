20bc2673a2b40828c981ad36ca9035bd39c9ff58
Lindroos Hsu <emptysmile1013@gmail.com>
2015-12-10 02:48:24 +0800

    commit the orignal form, can not be passed...

--- Algorithms/dynamic_programming/equal.go
+++ Algorithms/dynamic_programming/equal.go
@@ -2,14 +2,12 @@ package main
 
 import (
     "fmt"
-    "sync"
 )
 
 const (
     maxChocol   = 1001
     maxUint     = ^uint(0)
     maxInt      = int(maxUint >> 1)
-    maxLoop     = 6
 )
 
 func main() {
@@ -34,30 +32,15 @@ func main() {
         }
 
         answer := maxInt
-        cntCh := make(chan int, maxLoop)
-        var wg sync.WaitGroup
-        wg.Add(maxLoop)
-        for i := 0; i < maxLoop; i++ {
-            go func(i int) {
-                var counter int
-                for j := 0; j < N; j++ {
-                    v := chocols[j] - (minChocol - i)
-                    counter += v/5 + v%5/2 + v%5%2
-                }
+        for i := 0; i <= 5; i++ {
+            var counter int
+            for j := 0; j < N; j++ {
+                v := chocols[j] - (minChocol - i)
+                counter += v/5 + v%5/2 + v%5%2
+            }
 
-                cntCh <- counter
-                wg.Done()
-            }(i)
-        }
-
-        go func() {
-            wg.Wait()
-            close(cntCh)
-        }()
-
-        for a := range cntCh {
-            if a < answer {
-                answer = a
+            if counter < answer {
+                answer = counter
             }
         }
 
