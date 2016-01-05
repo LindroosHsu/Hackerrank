b62e4b86d35c73ed6704fef7b4346a492cc1b43a
Lindroos Hsu <emptysmile1013@gmail.com>
2016-01-05 23:16:44 +0800

    update

---
+++ Algorithms/search/task_scheduling.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    var T int
+    fmt.Scanf("%d\n", &T)
+
+    times := make(map[int]int)
+    // as max overshoot, the answer
+    times[0] = 0
+    maxD := 0
+    for t := 0; t < T; t++ {
+        var M, D int
+        fmt.Scanf("%d %d\n", &D, &M)
+
+        if D > maxD {
+            for i := D; i > maxD; i-- {
+                if _, ok := times[i]; !ok {
+                    // does not mean anything, but only mach map require
+                    times[i] = 1
+                    M--
+
+                    if M == 0 {
+                        break
+                    }
+                }
+            }
+        }
+
+        if M > 0 {
+            times[0] += M
+
+            if D > maxD {
+                maxD = D
+            }
+        }
+
+        fmt.Println(times[0])
+    }
+}
